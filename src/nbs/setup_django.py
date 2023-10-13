4c7960df153b29b3dd592f32bd3748d49e8b305a
oMarquess <pymasterclass@gmail.com>
2023-10-13 14:15:38 +0100

    ml-started

---
+++ src/nbs/setup_django.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+DJANGO_SETTINGS_MODULE = "recoengine.settings"
+PWD = os.getenv("PWD")
+
+def init():
+    os.chdir(PWD)
+    sys.path.insert(0, os.getenv("PWD"))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", DJANGO_SETTINGS_MODULE)
+    os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+    import django
+    django.setup()
